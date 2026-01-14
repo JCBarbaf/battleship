5d67c0d842d8963b201c8a86fa7af568bfb54471
jcbarbaf <jcbarbaf@gmail.com>
2026-01-14 12:33:57 +0100

    Console colors

--- boardController.java
+++ boardController.java
@@ -4,13 +4,17 @@ import java.util.Random;
 public class boardController {
     static char[][] CPUBoard;
     static char[][] playerBoard;
-    static Map<String, Integer> shipSizes = new HashMap<>(Map.of(
-        "carrier", 5,
-        "battleship", 4,
-        "destroyer", 3,
-        "submarine", 1
-    ));
+    static Map<String, Integer> shipSizes = new HashMap<>();
     static Random random;
+
+    //Console colors
+    public static final String BLUE_TEXT = "\u001B[34m";
+    public static final String GREEN_TEXT = "\u001B[32m";
+    public static final String RED_TEXT = "\u001B[31m";
+    public static final String YELLOW_TEXT = "\u001B[33m";
+    public static final String RESET_COLORS = "\u001B[0m";
+
+
     public static void init() {
         random = new Random();
         CPUBoard = new char[10][10];
@@ -25,25 +29,41 @@ public class boardController {
                 playerBoard[i][j] = '-';
             }
         }
+        shipSizes.put("carrier", 5);
+        shipSizes.put("battleship", 4);
+        shipSizes.put("destroyer", 3);
+        shipSizes.put("submarine", 1);
     }
     public static void PrintBoard () {
         PrintBoard("player");
     }
-    public static void PrintBoard (String boardType) {
+    public static void PrintBoard(String boardType) {
         char[][] board;
         if (boardType.equalsIgnoreCase("cpu")) {
             board = CPUBoard;
         } else {
             board = playerBoard;
         }
-        String boardString = "";
+        System.out.println("  A B C D E F G H I J");
         for (int i = 0; i < board.length; i++) {
+            System.out.print(i + " ");
             for (int j = 0; j < board[i].length; j++) {
-                boardString += board[i][j] + " ";
+                char letter = board[i][j];
+
+                if (letter == 'W' || letter == '-') {
+                    System.out.print(BLUE_TEXT);
+                    System.out.print(letter);
+                    System.out.print(RESET_COLORS);
+                    System.out.print(" ");
+                } else {
+                    System.out.print(GREEN_TEXT);
+                    System.out.print(letter);
+                    System.out.print(RESET_COLORS);
+                    System.out.print(" ");
+                }
             }
-            boardString += "\n";
+            System.out.println();
         }
-        System.out.println(boardString);
     }
 
     public static void SetBoard(String difficulty) throws Exception {
@@ -80,7 +100,7 @@ public class boardController {
                 if (vertical) {
                     for (int i = posInitMain; i < (posInitMain + size); i++) {
                         if (map[i][posInitSecond] != '-') {
-                            System.out.println("Not valid");
+                            // System.out.println("Not valid");
                             validPosition = false;
                         }
                     }
@@ -90,22 +110,22 @@ public class boardController {
                     for (int i = posInitMain; i < (posInitMain + size); i++) {
                         map[i][posInitSecond] = ship.toUpperCase().charAt(0);
                         if (posInitSecond - 1 >= 0) {
-                            map[i][posInitSecond - 1] = 'A';
+                            map[i][posInitSecond - 1] = 'W';
                         }
                         if (posInitSecond + 1 < map.length) {
-                            map[i][posInitSecond + 1] = 'A';
+                            map[i][posInitSecond + 1] = 'W';
                         }
                     }
                     if (posInitMain - 1 >= 0) {
-                        map[posInitMain - 1][posInitSecond] = 'A';
+                        map[posInitMain - 1][posInitSecond] = 'W';
                     }
                     if (posInitMain + size < map[0].length) {
-                        map[posInitMain + size][posInitSecond] = 'A';
+                        map[posInitMain + size][posInitSecond] = 'W';
                     }
                 } else {
                     for (int i = posInitMain; i < (posInitMain + size); i++) {
                         if (map[posInitSecond][i] != '-') {
-                            System.out.println("Not valid");
+                            // System.out.println("Not valid");
                             validPosition = false;
                         }
                     }
@@ -115,17 +135,17 @@ public class boardController {
                     for (int i = posInitMain; i < (posInitMain + size); i++) {
                         map[posInitSecond][i] = ship.toUpperCase().charAt(0);
                         if (posInitSecond - 1 >= 0) {
-                            map[posInitSecond - 1][i] = 'A';
+                            map[posInitSecond - 1][i] = 'W';
                         }
                         if (posInitSecond + 1 < map.length) {
-                            map[posInitSecond + 1][i] = 'A';
+                            map[posInitSecond + 1][i] = 'W';
                         }
                     }
                     if (posInitMain - 1 >= 0) {
-                        map[posInitSecond][posInitMain - 1] = 'A';
+                        map[posInitSecond][posInitMain - 1] = 'W';
                     }
                     if (posInitMain + size < map[0].length) {
-                        map[posInitSecond][posInitMain + size] = 'A';
+                        map[posInitSecond][posInitMain + size] = 'W';
                     }
                 }
             }
